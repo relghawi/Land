b431d42a7d6db6a4d0b7157b1b27f2a0f7f8a581
Lazaro Alonso <lazarus.alon@gmail.com>
2023-10-05 08:48:40 +0200

    bring back Reda's changes

--- src/SoilPlantAirContinuum/layers/measures.jl
+++ src/SoilPlantAirContinuum/layers/measures.jl
@@ -46,9 +46,89 @@ function T_VEG(spac::SPACMono{FT}) where {FT<:AbstractFloat}
         _t_veg += numerical∫(_iPS.g_lw, _iPS.LAIx) * (_iPS.p_sat - _iEN.p_H₂O) / _iEN.p_atm * _iPS.LA;
     end;
 
-    return _t_veg / spac.ga
+    return _t_veg / spac.ga  
 end
 
+
+function Canopy_cond(spac::SPACMono{FT}) where {FT<:AbstractFloat}
+    _g_lw::FT = 0;
+
+    for _i_can in 1:spac.n_canopy
+        # @show _i_can;
+        _iPS = spac.plant_ps[_i_can];
+        _g_lw += numerical∫(_iPS.g_lw, _iPS.LAIx);
+    end;
+
+    return  _g_lw / spac.ga
+ 
+end
+
+function An_out(spac::SPACMono{FT}) where {FT<:AbstractFloat}
+    _An::FT = 0;
+
+    for _i_can in 1:spac.n_canopy
+        _iPS = spac.plant_ps[_i_can];
+        _An += numerical∫(_iPS.An, _iPS.LAIx);
+    end;
+
+    return _An/ spac.ga
+end
+
+function LAIx_out(spac::SPACMono{FT}) where {FT<:AbstractFloat}
+    LAIx::FT = 0.0;
+
+    for _i_can in 1:spac.n_canopy
+        _iPS = spac.plant_ps[_i_can];
+        LAIx += numerical∫(_iPS.LAIx, _iPS.LAIx)
+    end;
+
+    return LAIx
+end
+
+
+function LA_out(spac::SPACMono{FT}) where {FT<:AbstractFloat}
+    LA::FT = 0;
+
+    for _i_can in 1:spac.n_canopy
+        _iPS = spac.plant_ps[_i_can];
+        LA =  _iPS.LA;
+    end;
+
+    return LA  
+end
+
+function p_sat_out(spac::SPACMono{FT}) where {FT<:AbstractFloat}
+    p_sat::FT = 0;
+
+    for _i_can in 1:spac.n_canopy
+        _iPS = spac.plant_ps[_i_can];
+        p_sat= _iPS.p_sat
+    end;
+
+    return p_sat
+end
+
+function p_H₂O_out(spac::SPACMono{FT}) where {FT<:AbstractFloat}
+    p_H₂O::FT = 0;
+
+    for _i_can in 1:spac.n_canopy
+        _iEN = spac.envirs[_i_can];
+        p_H₂O = _iEN.p_H₂O
+    end;
+
+    return p_H₂O  
+end
+
+function p_atm_out(spac::SPACMono{FT}) where {FT<:AbstractFloat}
+    p_atm::FT = 0;
+
+    for _i_can in 1:spac.n_canopy
+        _iEN = spac.envirs[_i_can];
+        p_atm=  _iEN.p_atm 
+    end;
+
+    return p_atm  
+end
 
 """
     PPAR(spac::SPACMono{FT}) where {FT<:AbstractFloat}
