0722dabdfd1b048dd7c4ecb0799870ebeec13853
Reda El Ghawi <relghawi@bgc-jena.mpg.de>
2023-10-09 20:42:18 +0200

    changes to empirical.jl

--- src/StomataModels/model/empirical.jl
+++ src/StomataModels/model/empirical.jl
@@ -168,10 +168,10 @@ end
 
 
 
-include("/Net/Groups/BGI/people/relghawi/Julia_hyb/Clima-Hyb2/DataUtils/prep_data.jl")
-include("/Net/Groups/BGI/people/relghawi/Julia_hyb/Clima-Hyb2/DataUtils/data.jl")
-include("/Net/Groups/BGI/people/relghawi/Julia_hyb/Clima-Hyb2/HybridModels/model_64.jl")
-include("/Net/Groups/BGI/people/relghawi/Julia_hyb/Clima-Hyb2/DataUtils/losses.jl")
+include("DataUtils/prep_data.jl")
+include("DataUtils/data.jl")
+include("HybridModels/model_64.jl")
+include("DataUtils/losses.jl")
 
 
 using Flux, JLD2
